4e45c20e47ae88260475792c389944146667c2af
Thinhbp <90796376+Thinhbp@users.noreply.github.com>
2023-01-12 15:15:12 +0700

    Update CreateNFT.sol

--- CreateNFT.sol
+++ CreateNFT.sol
@@ -1,10 +1,4 @@
-/**
- *Submitted for verification at Etherscan.io on 2022-07-20
-*/
 
-/**
- *Submitted for verification at BscScan.com on 2022-06-06
-*/
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
